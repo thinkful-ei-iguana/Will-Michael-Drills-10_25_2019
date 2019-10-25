32ec906cc1ab18f3b87bc8f4c39d30760e5f0e5f
Michael Weedman <michael.weedman@gmail.com>
2019-10-25 10:35:07 -0700

    little cleaning

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+/* eslint-disable no-undef */
 'use strict';
 
 const STORE = [
-  {id: cuid(), name: "apples", checked: false},
-  {id: cuid(), name: "oranges", checked: false},
-  {id: cuid(), name: "milk", checked: true},
-  {id: cuid(), name: "bread", checked: false}
+  {id: cuid(), name: 'apples', checked: false},
+  {id: cuid(), name: 'oranges', checked: false},
+  {id: cuid(), name: 'milk', checked: true},
+  {id: cuid(), name: 'bread', checked: false}
 ];
 
 
 function generateItemElement(item) {
   return `
     <li data-item-id="${item.id}">
-      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
@@ -25,11 +26,11 @@ function generateItemElement(item) {
 
 
 function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
+  console.log('Generating shopping list element');
 
   const items = shoppingList.map((item) => generateItemElement(item));
   
-  return items.join("");
+  return items.join('');
 }
 
 
@@ -105,7 +106,6 @@ function handleDeleteItemClicked() {
     deleteListItem(item);
     renderShoppingList();
   });
-  
 }
 
 // this function will be our callback when the page loads. it's responsible for
